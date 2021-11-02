3954b50818e3e9623aaf7c9ed7ba66a6fc45f53e
mooh2jj <mooh2jj@naver.com>
2021-11-02 13:37:02 +0900

    create system Customer component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
 
-const customer = {
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
   'name' : '도성곤',
   'birthday' : '911114',
   'gender' : '남자',
   'job' : '개발자'
-}
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name' : '홍길동',
+    'birthday' : '834514',
+    'gender' : '남자',
+    'job' : '난민'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name' : '남경필',
+    'birthday' : '9678814',
+    'gender' : '여자',
+    'job' : '정치인'
+    }
+]
 
 function App() {
   return (
     <div className="App">
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      {
+        customers.map(c =>{
+          return (
+            <Customer 
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -3,13 +3,37 @@ import React from "react";
 const Customer = (props) => {
     return (
         <div>
-            <h2>{props.name}</h2>
+            <CustomerProfile 
+                id={props.id}
+                image={props.image}
+                name={props.name}
+            />
+            <CustomerInfo 
+                birthday={props.birthday}
+                gender={props.gender}
+                job={props.job}
+            />
+        </div>
+    )
+}
+
+const CustomerProfile = (props) => {
+    return (
+        <div>
+            <img src={props.image} alt="profile" />
+            <h2>{props.name}({props.id})</h2>
+        </div>
+    )
+}
+
+const CustomerInfo = (props) => {
+    return (
+        <div>
             <p>{props.birthday}</p>
             <p>{props.gender}</p>
             <p>{props.job}</p>
         </div>
     )
 }
-
 
 export default Customer;
