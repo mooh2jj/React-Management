193d4fbfdfeed1c255349a2b9f51230e03e40d6f
mooh2jj <mooh2jj@naver.com>
2021-11-02 23:35:50 +0900

    apply material-ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
 
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+
+import { withStyles} from '@material-ui/core/styles'
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
 const customers = [
   {
   'id': 1,
@@ -30,8 +51,21 @@ const customers = [
 ]
 
 function App() {
+  const { classes } = this.props;
   return (
-    <div className="App">
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
       {
         customers.map(c =>{
           return (
@@ -47,8 +81,10 @@ function App() {
           )
         })
       }
-    </div>
+      </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,39 +1,49 @@
 import React from "react";
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 
 const Customer = (props) => {
     return (
-        <div>
-            <CustomerProfile 
-                id={props.id}
-                image={props.image}
-                name={props.name}
-            />
-            <CustomerInfo 
-                birthday={props.birthday}
-                gender={props.gender}
-                job={props.job}
-            />
-        </div>
+        // <div>
+        //     <CustomerProfile 
+        //         id={props.id}
+        //         image={props.image}
+        //         name={props.name}
+        //     />
+        //     <CustomerInfo 
+        //         birthday={props.birthday}
+        //         gender={props.gender}
+        //         job={props.job}
+        //     />
+        // </div>
+        <TableRow>
+            <TableCell>{props.id}</TableCell>
+            <TableCell><img src={props.image} alt="profile image"/></TableCell>
+            <TableCell>{props.name}</TableCell>
+            <TableCell>{props.birthday}</TableCell>
+            <TableCell>{props.gender}</TableCell>
+            <TableCell>{props.job}</TableCell>
+        </TableRow>
     )
 }
 
-const CustomerProfile = (props) => {
-    return (
-        <div>
-            <img src={props.image} alt="profile" />
-            <h2>{props.name}({props.id})</h2>
-        </div>
-    )
-}
+// const CustomerProfile = (props) => {
+//     return (
+//         <div>
+//             <img src={props.image} alt="profile" />
+//             <h2>{props.name}({props.id})</h2>
+//         </div>
+//     )
+// }
 
-const CustomerInfo = (props) => {
-    return (
-        <div>
-            <p>{props.birthday}</p>
-            <p>{props.gender}</p>
-            <p>{props.job}</p>
-        </div>
-    )
-}
+// const CustomerInfo = (props) => {
+//     return (
+//         <div>
+//             <p>{props.birthday}</p>
+//             <p>{props.gender}</p>
+//             <p>{props.job}</p>
+//         </div>
+//     )
+// }
 
 export default Customer;
